1c4b9b57fb6f1072c457173d5578785c17db72a5
dharmaku <dharmaku99@gmail.com>
2025-10-19 11:41:25 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+     <footer className='text-sm text-center italic'>
+      <p>&copy; {new Date().getFullYear()} DKFACE. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/app/components/Navheader.js
@@ -0,0 +1,11 @@
+function Navheader() {
+  return (
+     <header className="text-center">
+      <h1 className="text-4xl font-extrabold bg-gradient-to-r from-purple-300 via-red-400 to-pink-500 text-transparent bg-clip-text shadow-lg inline-block" style={{ filter: 'drop-shadow(0 0 10px rgba(235, 70, 150, 0.7))' }}>DKFACE | PREDICT YOUR FACE</h1>
+      <p>PREDICT YOUR FACE WITH Foto Atau Camera</p>
+    </header>
+
+  )
+}
+
+export default Navheader
